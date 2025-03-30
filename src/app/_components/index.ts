081a1003cde726e5b424606422e9c9fa57befdbd
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2025-03-30 11:50:54 -0300

    created Services component

--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -1,3 +1,4 @@
 export { AboutUs } from "./AboutUs"
 export { Header } from "./Header"
 export { Intro } from "./Intro"
+export { Services } from "./Services"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { Intro, Header, AboutUs } from "./_components"
+import { Intro, Header, AboutUs, Services } from "./_components"
 
 export default function Home() {
   return (
     <div className="ml-32 mr-32 mt-32">
-      <Header />
+      {/* <Header />
       <Intro />
-      <AboutUs />
+      <AboutUs /> */}
+      <Services />
     </div>
   )
 }
