bd49038131319efcf777dc0f8b7e5653922d177c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2025-04-20 14:46:48 -0300

    added images and gradietn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { Intro, Header, AboutUs, Services } from "./_components"
 
 export default function Home() {
   return (
-    <div className="ml-32 mr-32 mt-32">
-      {/* <Header />
+    <div className="px-32 pt-32 bg-gradient-to-b from-test1 from-70% via-test2 via-95% to-test1 to-100%">
+      <Header />
       <Intro />
-      <AboutUs /> */}
-      <Services />
+      <div className="gap-12 flex flex-col">
+        <AboutUs />
+        <Services />
+      </div>
     </div>
   )
 }
